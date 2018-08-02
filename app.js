922c10cb023672326409af213a2022de6f543036
Peter Krak <pkrak@csas.cz>
2018-08-02 11:40:26 +0200

    Error handling and CORS

--- app.js
+++ app.js
@@ -1,15 +1,71 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const hbs = require('hbs');
+const hbs = require('hbs');        // Handlebars
+const morgan = require('morgan');  // Logovací Middleware, který loguje, jak dlouho trval request
+const bodyParser = require('body-parser');
 
-// Import routes - user
+// Zapnutí logovacího middlewaru - všechny requesty jdou přes Morgan middleware
+//----------------------------------------------------------------------------------
+app.use(morgan('dev'));
+
+
+//nastavení parserů - prekladač struktury dat
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+//další MW - CORS
+app.use(function (req, res, next) {
+   res.header('Access-Control-Allow-Origin','*');
+   res.header('Access-Control-Allow-Headers','*');
+   if(req.method === 'OPTIONS'){
+       res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE');
+       return res.status(200).json({});
+   }
+   next();
+});
+
+
+
+// Import routes - řeš9 všechny requesty
+//--------------------------------------------------------------------------------------
 const usersRoutes = require('./API/routes/users');          // nastavujem jenom cestu k souboru cards.js
-app.use('/users', usersRoutes);
-
-
 const cardsRoutes = require('./API/routes/cards');    // nastavujem jenom cestu k souboru cards.js
+
+app.use('/users', usersRoutes);
 app.use('/cards', cardsRoutes);
+
+
+
+//Definice Homepage
+//-------------------------------------------------------------------------------------------
+app.use(express.static(__dirname ));
+
+app.get('/', function (req, res) {
+    res.render('Card.html');
+
+});
+
+
+//ERROR HANDLING - Mnou definovaný error - pro všechny věci, které hodí error v rámci funkce
+//-----------------------------------------------------------------------------------------
+app.use(function (req, res, next) {
+   const error = new Error('Volané API neexistuje');
+   error.status = 404;
+   next(error);
+ });
+
+//ERROR HANDLING - Pro všechny věci, které padnu na error ihned
+//------------------------------------------------------------------------------------------
+app.use(function (error, req, res, next) {
+    res.status(error.status || 500);
+        res.json({
+            error: {
+                message: error.message
+            }
+        });
+});
+
 
 module.exports = app;
 
@@ -24,11 +80,3 @@ hbs.registerPartials(__dirname + '/public/html/partials');
 
 
 
-//Definice Homepage
-//-------------------------------------------------------------------------------------------
-app.use(express.static(__dirname ));
-
-app.get('/', function (req, res) {
-    res.render('Card.html');
-
-});
